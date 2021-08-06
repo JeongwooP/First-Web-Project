dd87e86c31d06f4b3051443137f2154b0cbbdcef
JeongwooP <0204jw@gmail.com>
2021-08-07 00:14:53 +0900

    added all files

--- pythonprac/dbprac.py
+++ pythonprac/dbprac.py
@@ -10,25 +10,25 @@ same_ages = list(db.users.find({'age':21},{'_id':False}))
 
 # print(same_ages)
 
-user = db.users.find_one({'name':'bobby'})
+# user = db.users.find_one({'name':'bobby'})
 # print(user)
 
 # db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
 
-db.users.delete_one({'name':'bobby'})
-
-# # 저장 - 예시
-# doc = {'name':'bobby','age':21}
-# db.users.insert_one(doc)
-#
-# # 한 개 찾기 - 예시
-# user = db.users.find_one({'name':'bobby'})
-#
-# # 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
-# same_ages = list(db.users.find({'age':21},{'_id':False}))
-#
-# # 바꾸기 - 예시
-# db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
-#
-# # 지우기 - 예시
 # db.users.delete_one({'name':'bobby'})
+
+# 저장 - 예시
+doc = {'name':'bobby','age':21}
+db.users.insert_one(doc)
+
+# 한 개 찾기 - 예시
+user = db.users.find_one({'name':'bobby'})
+
+# 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
+same_ages = list(db.users.find({'age':21},{'_id':False}))
+
+# 바꾸기 - 예시
+db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
+
+# 지우기 - 예시
+db.users.delete_one({'name':'bobby'})
